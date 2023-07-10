800b0f18eb558190185b109060cf49e5ca5705ca
FizzyWhizbang <marc.levine@gmail.com>
2023-07-10 13:48:29 -0500

    Update passgen.go

--- passgen.go
+++ passgen.go
@@ -6,19 +6,22 @@ import (
 	"math/big"
 )
 
+// characters for password
 const Chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+,.?/:;{}[]`~"
 
 func main() {
+	//create flag for length
 	length := flag.Int("l", 20, "password length")
 	flag.Parse()
 
 	println(genPass(*length))
 }
 
+// generate password
 func genPass(length int) string {
 	max := len(Chars)
 	pass := ""
-
+	//loop the length/number of characters required for password
 	for i := 0; i < length; i++ {
 		n, _ := rand.Int(rand.Reader, big.NewInt(int64(max)))
 		pass = pass + string(Chars[n.Int64()])
