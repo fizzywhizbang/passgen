80c5bbbd7700b5ab32eeb0610b9b2368c5ea21e1
FizzyWhizbang <marc.levine@gmail.com>
2023-07-13 08:11:27 -0500

    Update passgen.go

--- passgen.go
+++ passgen.go
@@ -3,25 +3,26 @@ package main
 import (
 	"crypto/rand"
 	"flag"
+	"fmt"
 	"math/big"
 )
 
-// characters for password
+// allowed characters for password
 const Chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+,.?/:;{}[]`~"
 
 func main() {
 	//create flag for length
 	length := flag.Int("l", 20, "password length")
 	flag.Parse()
+	fmt.Println(genPass(*length))
 
-	println(genPass(*length))
 }
 
 // generate password
 func genPass(length int) string {
 	max := len(Chars)
 	pass := ""
-	//loop the length/number of characters required for password
+	//iterate over the length/number of characters required for password
 	for i := 0; i < length; i++ {
 		n, _ := rand.Int(rand.Reader, big.NewInt(int64(max)))
 		pass = pass + string(Chars[n.Int64()])
